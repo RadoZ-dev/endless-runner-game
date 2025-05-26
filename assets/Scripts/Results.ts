7429dc49e1f378a6e7052b040d1e4ba59a569e9a
RadoZ-dev <rado.jazzfusion@gmail.com>
2025-05-26 21:51:24 +0200

    results script init

---
+++ assets/Scripts/Results.ts
@@ -0,0 +1,50 @@
+import { _decorator, Component, Node, Label } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('Results')
+export class Results extends Component {
+    @property({
+        type: Label
+    })
+    public score: Label;
+
+    @property({
+        type: Label
+    })
+    public highScore: Label;
+
+    @property({
+        type: Label
+    })
+    public resultFinal: Label;
+
+    maxScore: number = 0; 
+    currentScore: number;
+
+    updateScore(currentScore: number) {
+        this.currentScore = currentScore;
+        this.score.string = (`Score: ${this.currentScore}`);
+    }
+
+    resetScore() {
+        this.updateScore(0);
+        this.hideResults();
+    }
+
+    increaseScore() {
+        this.updateScore(this.currentScore + 1);
+    }
+
+    displayResults() {
+        this.maxScore = Math.max(this.currentScore, this.maxScore);
+        this.highScore.string = (`High Score: ${this.maxScore}`);
+        this.resultFinal.node.active = true;
+    }
+
+    hideResults() {
+        this.resultFinal.node.active = false;
+        this.highScore.node.active = false;
+    }
+}
+
+
